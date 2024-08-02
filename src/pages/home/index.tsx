29b266173150cf89124d724fc8c719cd735ae19e
gabriel natan <gabriel.souza@groupwork.com.br>
2024-08-02 13:23:15 -0300

    maneiro

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export const Home = ()=>{
+    return(
+        <div>
+            home
+            <Link to="/login">LOGIN</Link>
+        </div>
+    )
+}
