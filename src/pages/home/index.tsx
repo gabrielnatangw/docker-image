1aaf8e223d7e75e543d461c65baf4c48cff10b05
Gabriel Natan <gabriel--natan@live.com>
2024-08-04 18:48:03 -0300

    aaaaaaa

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const Home = ()=>{
     return(
         <div>
-            home
+            muda pra ver
             <Link to="/login">LOGIN</Link>
         </div>
     )
